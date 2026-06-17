d59c3bbb17646c4679d660bf484d48b84f62d5cd
kanikepreethi98-source <kanikepreethi98@gmail.com>
2026-06-17 21:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,38 +13,40 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file)
 
     feature_columns = [
-    'State',
-    'Account length',
-    'Area code',
-    'International plan',
-    'Voice mail plan',
-    'Number vmail messages',
-    'Total day minutes',
-    'Total day calls',
-    'Total day charge',
-    'Total eve minutes',
-    'Total eve calls',
-    'Total eve charge',
-    'Total night minutes',
-    'Total night calls',
-    'Total night charge',
-    'Total intl minutes',
-    'Total intl calls',
-    'Total intl charge'
-]
+        'State',
+        'Account length',
+        'Area code',
+        'International plan',
+        'Voice mail plan',
+        'Number vmail messages',
+        'Total day minutes',
+        'Total day calls',
+        'Total day charge',
+        'Total eve minutes',
+        'Total eve calls',
+        'Total eve charge',
+        'Total night minutes',
+        'Total night calls',
+        'Total night charge',
+        'Total intl minutes',
+        'Total intl calls',
+        'Total intl charge'
+    ]
 
-X = df[feature_columns]
+    X = df[feature_columns]
 
-scaled_data = scaler.transform(X)
+    scaled_data = scaler.transform(X)
     predictions = model.predict(scaled_data)
 
     df["Prediction"] = predictions
+
     st.write(df)
 
     csv = df.to_csv(index=False).encode("utf-8")
+
     st.download_button(
         "Download Results",
         csv,
         "predictions.csv",
-        "text/csv",
+        "text/csv"
     )
